e46ebf29afcd0b74f82daece745ef9c8ac4bc3fc
Jeremiah Chukwuka <jeremiahchukwu@gmail.com>
2020-06-09 05:07:14 +0100

    script update

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+git pull origin
+git checkout dev
+docker stop php && docker rm php && docker rmi phpimageresize:latest
 docker build -t phpimageresize .
 docker run -d -p 8090:8080 --name php phpimageresize:latest
 
