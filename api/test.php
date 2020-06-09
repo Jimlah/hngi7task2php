0e0fc6ce4b613f7cb7741a853967f70a36e395e3
iammutevu <ianmutevu96@gmail.com>
2020-06-09 18:01:58 +0300

    API Complete

--- api/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include_once('resize.php');
-
-
-$object = new ImageResizer();
-$object->downloadImage("https://upload.wikimedia.org/wikipedia/commons/1/14/Rodney_boat.jpg");
--- src/api/index.php
+++ src/api/index.php
@@ -1,11 +1,13 @@
 <?php
+ini_set('display_errors', 1);
+
 // required headers
 header('Access-Control-Allow-Origin: *');
 header("Content-Type: application/json");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Headers");
 
-include_once('resize.php');
+include_once('ImageResizer.php');
 
 //read content of request from POST request
 $data = json_decode(file_get_contents("php://input"));
@@ -18,15 +20,10 @@ $resize_width = $data->width;
 $resize_height = $data->height;
 
 
-//process downloaded image
+//process downloaded image and return JSON response
 $object->processImage($image_url, $resize_width, $resize_height);
 
-/**
-echo json_encode(
-	array("image_url"=> $imageName)
-);
 
-**/
 
 
 
