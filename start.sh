874d4123358256abd96e64fa22c04d42bb02f8f8
youngxian <38491485+youngxian@users.noreply.github.com>
2020-06-11 19:04:10 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 git pull origin
 git checkout dev
-docker stop teamflash && docker rm teamflash && docker rmi teamflash:latest
-docker build -t teamflash .
-docker run -d -p 8090:8080 --name teamflash teamflash:latest
+docker stop teamflashphp && docker rm teamflashphp && docker rmi teamflashphp:latest
+docker build -t teamflashphp .
+docker run -d -p 1060:8080 --name teamflashphp teamflashphp:latest
+
 
 
 
